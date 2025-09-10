ac457ad57fbf02459ace3562dc49a62a1a8ec160
Ansari Mohsin Ahmed <mohsinahmedansari99@gmail.com>
2025-09-10 23:47:02 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello Mohsin!");
+
+string firstFriend = "Adnan";
+string secondFriend = "Kutbuddin";
+
+Console.WriteLine($"My friends are {firstFriend} and {secondFriend}");
