ba51dca1415898d78c7db2ba67ac3b59563cf979
Ansari Mohsin Ahmed <mohsinahmedansari99@gmail.com>
2025-09-11 00:00:19 +0530

    Name changed

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello Mohsin!");
 
-string firstFriend = "Adnan1";
+string firstFriend = "Adnan";
 string secondFriend = "Kutbuddin";
 
 Console.WriteLine($"My friends are {firstFriend} and {secondFriend}");
