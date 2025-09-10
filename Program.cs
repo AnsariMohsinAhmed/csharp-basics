a44fc401a4286fadb32be3167cf6a07d79e57101
Ansari Mohsin Ahmed <mohsinahmedansari99@gmail.com>
2025-09-10 23:59:02 +0530

    Initial commit

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello Mohsin!");
 
-string firstFriend = "Adnan";
+string firstFriend = "Adnan1";
 string secondFriend = "Kutbuddin";
 
 Console.WriteLine($"My friends are {firstFriend} and {secondFriend}");
