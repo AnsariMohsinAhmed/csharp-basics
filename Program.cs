f2377f54f3903250f9d0e7161c3f53c94950c157
Ansari Mohsin Ahmed <mohsinahmedansari99@gmail.com>
2025-09-11 23:27:34 +0530

    Working with strings

--- Program.cs
+++ Program.cs
@@ -1,10 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello Mohsin!");
 
-string firstFriend = "     Adnan     ";
+string firstFriend = "Adnan";
 firstFriend = firstFriend.Trim();
-string secondFriend = "    Kutbuddin    ";
+string secondFriend = "Kutbuddin";
 
-Console.WriteLine($"My friends are {firstFriend} and {secondFriend.Trim()}"); // string interpolation
+string friends = $"My friends are {firstFriend} and {secondFriend.Trim()}";
+Console.WriteLine(friends.Replace("Kutbuddin", "Saad")); // string interpolation
 
 // Console.WriteLine("My friends are " +  firstFriend + " and " + secondFriend); // concatenation
