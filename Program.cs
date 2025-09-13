52c6f87906c1e4d44fbece5013b18fcf7be867d5
Ansari Mohsin Ahmed <mohsinahmedansari99@gmail.com>
2025-09-13 15:13:51 +0530

    Working with Numbers and Integers

--- Program.cs
+++ Program.cs
@@ -8,9 +8,19 @@ firstFriend = firstFriend.Trim();
 string secondFriend = "Kutbuddin";
 
 string friends = $"My friends are {firstFriend} and {secondFriend.Trim()}"; // string interpolation
-Console.WriteLine(friends.Contains("Kutbuddin"));
-Console.WriteLine($"Length of friends string :- {friends.Length}");
-Console.WriteLine(friends.StartsWith("My"));
+Console.WriteLine(friends);
+//Console.WriteLine(friends.Contains("Kutbuddin"));
+//Console.WriteLine($"Length of friends string :- {friends.Length}");
+//Console.WriteLine(friends.StartsWith("My"));
 //friends = friends.Replace("Kutbuddin", "Saad"); // Explicitly assigning new values to 'friends' string
 
 // Console.WriteLine("My friends are " +  firstFriend + " and " + secondFriend); // concatenation
+
+
+// Numbers, Integers and Math
+//int a = 18;
+//int b = 6;
+int a = 2100000000;
+int b = 2100000000;
+long c = checked(a + b);
+Console.WriteLine(c);
