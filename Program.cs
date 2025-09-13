5faad5c6a5a135618166c88a2a4b06168eb5d90c
Ansari Mohsin Ahmed <mohsinahmedansari99@gmail.com>
2025-09-13 12:13:28 +0530

    Strings basics done

--- Program.cs
+++ Program.cs
@@ -1,11 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
+// Strings
+// Strings are immutable in .NET
 Console.WriteLine("Hello Mohsin!");
 
 string firstFriend = "Adnan";
 firstFriend = firstFriend.Trim();
 string secondFriend = "Kutbuddin";
 
-string friends = $"My friends are {firstFriend} and {secondFriend.Trim()}";
-Console.WriteLine(friends.Replace("Kutbuddin", "Saad")); // string interpolation
+string friends = $"My friends are {firstFriend} and {secondFriend.Trim()}"; // string interpolation
+Console.WriteLine(friends.Contains("Kutbuddin"));
+Console.WriteLine($"Length of friends string :- {friends.Length}");
+Console.WriteLine(friends.StartsWith("My"));
+//friends = friends.Replace("Kutbuddin", "Saad"); // Explicitly assigning new values to 'friends' string
 
 // Console.WriteLine("My friends are " +  firstFriend + " and " + secondFriend); // concatenation
