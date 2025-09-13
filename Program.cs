a54dab2f70eae8f60cf021b232b65e9bc24982bf
Ansari Mohsin Ahmed <mohsinahmedansari99@gmail.com>
2025-09-13 16:03:38 +0530

    Explicit type conversion

--- Program.cs
+++ Program.cs
@@ -16,11 +16,26 @@ Console.WriteLine(friends);
 
 // Console.WriteLine("My friends are " +  firstFriend + " and " + secondFriend); // concatenation
 
+/* ----------------------- BREAK -----------------------  */
 
 // Numbers, Integers and Math
 //int a = 18;
 //int b = 6;
-int a = 2100000000;
-int b = 2100000000;
-long c = checked(a + b);
+
+// long data type with explicit conversion
+//int a = 2100000000;
+//int b = 2100000000;
+//long c = (long)a + (long)b; // explicitly telling C# compiler to use long data type arithmetic for addition otherwise compiler will use int arithmetic
+//Console.WriteLine(c);
+
+// checked() will throw exception at runtime
+//int a = 2100000000;
+//int b = 2100000000;
+//long c = checked(a + b);
+//Console.WriteLine(c);
+
+// explicit conversion example
+int a = (int)42.1;
+int b = (int)38.2;
+long c = checked(a + b); // 80.30000000000001 changed to 80 because of explicit int conversion
 Console.WriteLine(c);
