01a604049dba9c5e65e0a54e7a27503463d27dac
AnsariMohsinAhmed <mohsinahmedansari99@gmail.com>
2025-10-03 10:11:45 +0530

    Working with LINQ

---
+++ LINQOperations.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace helloworld
+{
+    internal class LINQOperations
+    {
+        public static void LINQExamples()
+        {
+            // This is data source
+            int[] scores = [97, 92, 81, 60];
+
+            // Defining the query expression below
+            IEnumerable<int> scoreQuery =
+                from score in scores
+                where score > 80
+                select score;
+
+            // Execute the query
+            foreach (int item in scoreQuery)
+            {
+                Console.WriteLine($"{item} ");
+            }
+            Console.WriteLine(scoreQuery);
+        }
+
+        public static List<int> GetScoresGreaterThan50(int[] scoresArray)
+        {
+            List<int> result = new List<int>();
+            // query expression
+            IEnumerable<int> scoreQuery =
+                from score in scoresArray
+                where score > 50
+                select score;
+
+            // query execution
+            foreach (int item in scoreQuery)
+            {
+                result.Add(item);
+            }
+            return result;
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -22,14 +22,30 @@ using helloworld;
 
 // exercise
 // sum of all integers from 1 to 20 that are divisible by 3
-Exercises.Sumof1to20NumbersDivisibleBy3();
+//Exercises.Sumof1to20NumbersDivisibleBy3();
 
 // List
 //ListExercise.ListExercises();
 
-List<int> result = Exercises.fibonacciSeriesTill20();
-Console.Write("The fibonacci series till 20 is as follows :- ");
-foreach (var item in result)
+//List<int> result = Exercises.fibonacciSeriesTill20();
+//Console.Write("The fibonacci series till 20 is as follows :- ");
+//foreach (var item in result)
+//{
+//    Console.Write($"{item}, ");
+//}
+
+// LINQ
+
+//LINQOperations.LINQExamples();
+int[] scores = [55, 44, 84, 30];
+Console.WriteLine("Scores array is below:- ");
+foreach (int item in scores)
 {
     Console.Write($"{item}, ");
+}
+List<int> result =  LINQOperations.GetScoresGreaterThan50(scores);
+Console.WriteLine($"\nScores greater than 50 are :- ");
+foreach (int item in result)
+{
+    Console.Write($"{item} ");
 }
